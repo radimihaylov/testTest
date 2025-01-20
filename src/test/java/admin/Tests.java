9f91186d14f3b4f3da99a833a24d98983a2008d4
Radoslav Mihaylov <radimihaylov@gmail.com>
2025-01-20 04:31:54 +0200

    some changes but .... no success

--- src/test/java/admin/Tests.java
+++ src/test/java/admin/Tests.java
@@ -29,8 +29,12 @@ public class Tests extends BaseTest {
     @Test
     public void check() {
         driver.get("https://forms.clickup.com/2314027/p/f/26ktb-6387/56LKNUZ9BDYXSC73SY/unlock-your-automation-potentialwitha-free-framework-assessment");
+        driver.switchTo().frame("c-1bbbpxln36qf");
         WebElement element = driver.findElement(By.id("cu-form-control-0"));
         element.click();
+        driver.switchTo().defaultContent();
+//        WebElement element = driver.findElement(By.id("cu-form-control-0"));
+//        element.click();
 
 
     }
